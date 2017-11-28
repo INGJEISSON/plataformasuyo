eca8b56507d09c4d987e1a84fa41c9cf29191683
jeisson <jibarguen@suyo.co>
2017-11-28 18:40:20 -0500

    Cambiando parámetros de google

--- includes/dependencia/conexion.php
+++ includes/dependencia/conexion.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-if($_SERVER['REMOTE_ADDR']=='http://localhost/plataformasuyo/index.php')
+$remote_host="http://".$_SERVER['HTTP_HOST'].$_SERVER['PHP_SELF'];
+if($remote_host=='http://localhost/plataformasuyo/index2.php')
 $conexion=mysqli_connect("localhost","root","", "plataformasuyo");
-elseif($_SERVER['REMOTE_ADDR']=='http://platform.suyo.io/index.php')
+elseif($remote_host=='http://platform.suyo.io/index.php')
 $conexion=mysqli_connect("bdplatform.c7wzmctni82t.us-west-2.rds.amazonaws.com:3306","as_suyoplatf", "s.col2017qkuk", "suyoplataf");
