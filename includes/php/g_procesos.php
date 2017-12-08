b0415463f47e481559983eec398304bee8b74e16
jeisson <jibarguen@suyo.co>
2017-12-08 14:32:33 -0500

    Directorio files

--- includes/php/g_procesos.php
+++ includes/php/g_procesos.php
@@ -13,7 +13,7 @@ if(!empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQU
     //comprobamos si existe un directorio para subir el archivo
     //si no es así, lo creamos
     if(!is_dir("files/")) 
-        mkdir("files/", 0777);
+        @mkdir("files/", 0777);
      
     //comprobamos si el archivo ha subido
     if ($file && move_uploaded_file($_FILES['archivo']['tmp_name'],"../files/".$file))
