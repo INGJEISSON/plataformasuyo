bc0672451dd36edfa0be9182c8bcee1625218447
jeisson <jibarguen@suyo.co>
2018-01-31 08:34:34 -0500

    Documento diagnóstico

--- includes/php/reportes/pdfdiagnostico.php
+++ includes/php/reportes/pdfdiagnostico.php
@@ -7,9 +7,7 @@
 .fuente_letra {
 	font-family: "Trebuchet MS", Arial, Helvetica, sans-serif;
 }
-.fuente_letra table tr td {
-	color: #5b5b5f;
-}
+
 .gris_texto{
 		color: #5b5b5f;
 		font-size:11px
@@ -35,6 +33,19 @@
 .verde{
 	color: #96C832;
 	
+}
+.tabla_tecnico{
+	background-image:url(img/Imagen1.png);
+	background-position:center;
+	background-repeat:no-repeat;
+	text-align:center
+
+}
+.tabla_tecnico2{
+	background-image:url(img/Marco_Texto.png);	
+	background-repeat:no-repeat;
+	background-size:1024px 60px;
+
 }
 </style>
 </head>
@@ -107,26 +118,398 @@
   </tr>
 </table>
 <p>&nbsp;</p>
-<table width="384" border="0">
+<table width="280" border="0" cellpadding="0" cellspacing="0">
   <tr>sasa
-    <td width="32" rowspan="5"><img src="img/Barra_Subtitulo.png" width="23" height="77" /></td>
-    <td width="342"><p class="azul">01 ESTADO ACTUAL DEL USUARIO Y EL PREDIO<br />
+    <td width="35" rowspan="3"><img src="img/Barra_Subtitulo.png" width="23" height="111" /></td>
+    <td width="245"><p class="azul" style="font-size:40px">01<br />
     </p></td>
   </tr>
   <tr>
-    <td><p class="turquesa">Sr.</p></td>
+    <td><p class="azul" style="font-size:24px; font-weight: bold">ESTADO ACTUAL </span></p></td>
   </tr>
   <tr>
-    <td><p class="gris_texto">Calle 13 # 10 -33</p></td>
+    <td height="23"><p class="gris_texto"><span class="azul" style="font-size:17px; bold">DEL USUARIO Y EL PREDIO</span></p></td>
+  </tr>
+</table>
+<table width="456" border="0">
+  <tr>
+    <td><p class="azul_clarol">USUARIO</p></td>
+    <td><p class="azul_clarol">IDENTIFICACIÓN</p></td>
+    <td>&nbsp;</td>
   </tr>
   <tr>
-    <td ><p class="gris_texto">Barranquilla, Colombia</p></td>
+    <td><p class="gris_texto"></p></td>
+    <td><p class="gris_texto"></p></td>
+    <td>&nbsp;</td>
   </tr>
   <tr>
+    <td><p class="azul_clarol">DIRECCIÓN</p></td>
+    <td><p class="azul_clarol">BARRIO</p></td>
+    <td><p class="azul_clarol">MUNICIPIO</p></td>
+  </tr>
+  <tr>
+    <td><p class="gris_texto"></p></td>
+    <td><p class="gris_texto"></p></td>
+    <td><p class="gris_texto"></p></td>
+  </tr>
+  <tr>
+    <td><p class="azul_clarol">FOLIO MATRÍCULA</p></td>
+    <td><p class="azul_clarol">REFERENCIA CATASTRAL</p></td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td><p class="gris_texto"></p></td>
+    <td><p class="gris_texto"></p></td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+<p class="azul" style="font-size:15px; font-weight:bold">NECESIDAD IDENTIFICADA</p>
+<table width="603" border="0">
+  <tr>
+    <td width="12" height="162"><img src="img/Barra_Parrafo1.png" alt="" width="10" height="153" /></td>
+    <td width="330"><p class="gris_texto">Por medio de la presente nos permitimos en<br />
+      primera medida agradecer la confianza que<br />
+      ha depositado en Suyo y en todo el grupo de profesionales que hay detrás de esta empre-<br />
+      sa de impacto social. Respondiendo al servicio que adquirió con nosotros, hacemos 
+      entrega de su documento de diagnóstico de        propiedad en el cual resumimos el análisis        legal y técnico que hemos realizado de su        caso; así mismo señalamos la viabilidad y el        tipo de servicio que requiere, con todos los    costos asociados. Recuerde preguntar a su</p></td>
+    <td width="247"><p class="gris_texto">asesor de ventas por las diferentes opciones<br />
+      de financiación que brindamos a través de<br />
+      nuestros aliados.<br />
+      Finalmente, quisiera reiterarle que sabemos<br />
+      que su hogar es su bien más preciado, por<br />
+      esto no defraudaremos su voto de confianza.<br />
+      Esperamos que pueda tomar los servicios<br />
+      recomendados con nosotros, Suyo y todo su<br />
+      equipo humano estará esperándole para<br />
+      asesorarlo y guiarlo de forma personalizada.</p></td>
+  </tr>
+</table>
+<p style="font-style:italic; font-size:11px">A continuación presentamos el análisis de su caso y las posibilidades de servicios para darle respuesta a sus deseso y, en caso de no ser posible, las diferentes alternativas que le podemos brindar</p>
+<p class="azul" style="font-size:15px; font-weight:bold">SITUACIÓN ACTUAL</p>
+<p class="turquesa" style="font-size:11px; font-weight:bold">DESCRIPCIÓN DEL PREDIO Y DE LA CONSTRUCCIÓN</p>
+<table width="200" border="0">
+  <tr>
+    <td><p class="gris_texto" style="font-size:11px;">Dsafdadad</p>
+</td>
+  </tr>
+</table>
+<table width="782" border="0">
+  <tr>
+    <td width="70"><span class="turquesa" style="font-size:11px; font-weight:bold"><img src="img/Barra_Foto.png" alt="" width="70" height="143" /></span></td>
+    <td width="702">&nbsp;</td>
+  </tr>
+</table>
+<p class="turquesa" style="font-size:11px; font-weight:bold"><span class="turquesa" style="font-size:11px; font-weight:bold">Análisis del cumplimiento del plan de ordanimiento territorial con respecto a las características ambientales:</span></p>
+<table width="200" border="0">
+  <tr>
+    <td width="19">&nbsp;</td>
+    <td width="171"><p class="gris_texto" style="font-size:11px; font-weight:bold">&nbsp;</p></td>
+  </tr>
+</table>
+<p class="turquesa" style="font-size:11px; font-weight:bold"><span class="turquesa" style="font-size:11px; font-weight:bold">Análisis del cumplimiento del plan de ordanimiento territorial con respecto a las características físicas de la propiedad:</span></p>
+<table width="782" height="557" border="0" class="tabla_tecnico">
+  <tr>
+    <td width="176">dsadda</td>
+    <td width="333" style="text-align:center">dasdadadadad</td>
+    <td width="259">&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td><p>dadasdadada</p></td>
+  </tr>
+  <tr>
+    <td height="115"><p>dasdadada</p>
+    <p>&nbsp;</p>
+    <p>&nbsp;</p></td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>dsadadadasd</td>
+    <td>&nbsp;</td>
+    <td>dsaddadadadadda</td>
+  </tr>
+  <tr>
+    <td>adsadaddadda</td>
+    <td>dsadadadadadad</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+<table width="746" border="0" class="gris_texto" style="font-weight:bold">
+  <tr>
+    <td>CARACTERÍSTICAS</td>
+    <td>PREDIO DEL USUARIO</td>
+    <td>EXIGENCIAS DEL POT</td>
+    <td>CUMPLIMIENTO DE LAS EXIGENCIAS</td>
+  </tr>
+</table>
+<table width="735" height="96" border="0" class="tabla_tecnico2">
+  <tr>
+    <td height="92"><p>sdsdsdmnmnmnmnm</p></td>
+  </tr>
+</table>
+<p><span class="turquesa" style="font-size:11px; font-weight:bold">TITULARIDAD DEL PREDIO</span></p>
+<table width="200" border="0" class="gris_texto">
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+<p><span class="turquesa" style="font-size:11px; font-weight:bold">FORMA COMO ADQUIRIO EL PREDIO POR EL USUARIO</span></p>
+<table width="200" border="0" class="gris_texto">
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+<p><span class="turquesa" style="font-size:11px; font-weight:bold">OTRAS SITUACIONES RELACIONADAS CON EL PREDIO Y LA CONSTRUCCION</span></p>
+<table width="200" border="0" class="gris_texto">
+  <tr>
+    <td>&nbsp;</td>
+    <td>&nbsp;</td>
+  </tr>
+</table>
+<table width="280" border="0" cellpadding="0" cellspacing="0">
+  <tr>sasa
+    <td width="35" rowspan="3"><img src="img/Barra_Subtitulo.png" alt="" width="23" height="111" /></td>
+    <td width="245"><p class="azul" style="font-size:40px">02<br />
+    </p></td>
+  </tr>
+  <tr>
+    <td><p class="azul" style="font-size:24px; font-weight: bold">SERVICIOS</p></td>
+  </tr>
+  <tr>
+    <td height="23"><p class="gris_texto"><span class="azul" style="font-size:17px; bold">OFRECIDOS</span></p></td>
+  </tr>
+</table>
+<p>Teniendo en cuenta la situación actual y lo que el señor desea, le recomendamos los siguientes servicios</p>
+<p>&nbsp;</p>
+<table width="280" border="0" cellpadding="0" cellspacing="0">
+  <tr>sasa
+    <td width="35" rowspan="3"><img src="img/Barra_Subtitulo.png" alt="" width="23" height="111" /></td>
+    <td width="245"><p class="azul" style="font-size:40px">03<br />
+    </p></td>
+  </tr>
+  <tr>
+    <td><p class="azul" style="font-size:24px; font-weight: bold">INVERSIÓN</p></td>
+  </tr>
+  <tr>
+    <td height="23"><p class="gris_texto"><span class="azul" style="font-size:17px; bold">VALOR Y CONDICIONES</span></p></td>
+  </tr>
+</table>
+<p><span class="azul" style="font-size:15px; font-weight:bold">COTIZACIÓN</span></p>
+<table width="603" border="0">
+  <tr>
+    <td width="12" height="162"><img src="img/Barra_Parrafo1.png" alt="" width="10" height="153" /></td>
+    <td width="330"><p class="gris_texto">Suyo ofrece sus servicios en dos  modalidades, la primera en donde se  incluye sólo la asesoría, análisis y  acompañamiento legal y técnico, así como  la preparación de documentos  relacionados con el servicio final. Sin  embargo, en este caso es el cliente quien  se hace responsable tanto por realizar  personalmente los trámites relacionados,  como por hacer el pago de los costos  asociados, ante cada entidad  gubernamental correspondiente. En esta  opción el tiempo requerido para realizar  el servicio dependerá en buena parte del  cliente, dado que sería directamente el </p></td>
+    <td width="247"><p class="gris_texto">responsable de realizar los trámites del  proceso.  La segunda modalidad que incluye toda la  asesoría, análisis y acompañamiento de la  opción 1, además del pago de todos los  trámites, costos gubernamentales y  judiciales, e impuestos del servicio (si  aplican). De igual forma, Suyo realizará  directamente todos los trámites  personalmente. En esta opción, sí podemos  estimar el tiempo requerido del servicio,  dado que la misma empresa realiza los  trámites.</p></td>
+  </tr>
+</table>
+<p><span style="font-style:italic; font-size:11px">A continuación se resumen los costos para una y otra modalidad de los servicios ofrecidos. Para más detalle de los costos consulte el anexo.</span></p>
+<table width="645" border="0" class="gris_texto">
+  <tr style="font-weight:bold">
+    <td width="243">SERVICIO</td>
+    <td width="170">COSTOS MODALIDAD 1 (SÓLO ASESORÍA)</td>
+    <td width="218">COSTOS MODALIDAD 2:  SERVICIO COMPLETO  (Asesoría, Análisis,  Acompañamiento y Trámites) </td>
+  </tr>
+  <tr>
+    <td>&nbsp;</td>
+    <td>$</td>
+    <td>$</td>
+  </tr>
+  <tr class="azul" style="font-weight:bold;">
+    <td>TOTAL</td>
+    <td>$</td>
+    <td>$</td>
+  </tr>
+  <tr style="font-weight:bold; font-style:italic; text-align:right">
+    <td height="18" colspan="2">&nbsp;</td>
+    <td>*Precios con IVA Incluido</td>
+  </tr>
+</table>
+<p>&nbsp;</p>
+<table width="364" border="0" class="azul">
+  <tr>
+    <td style="font-weight:bold">ELEMENTOS QUE DECIDE TENER EN CUENTA</td>
+  </tr>
+  <tr>
+    <td>SI DECIDE ADQUIRIR EL SERVICIO</td>
+  </tr>
+</table>
+<table width="280" border="0" cellpadding="0" cellspacing="0">
+  <tr>sasa
+    <td width="35" rowspan="3"><img src="img/Barra_Subtitulo.png" alt="" width="23" height="111" /></td>
+    <td width="245"><p class="azul" style="font-size:40px">04<br />
+    </p></td>
+  </tr>
+  <tr>
+    <td><p class="azul" style="font-size:24px; font-weight: bold">GARANTÍAS</p></td>
+  </tr>
+  <tr>
+    <td height="23"><p class="gris_texto"><span class="azul" style="font-size:17px; bold">Y DEVOLUCIONES</span></p></td>
+  </tr>
+</table>
+<table width="364" border="0" class="azul">
+  <tr>
+    <td style="font-weight:bold">POLÍTICA DE</td>
+  </tr>
+  <tr>
+    <td>DEVOLUCIONES SUYO</td>
+  </tr>
+</table>
+<table width="549" border="0">
+  <tr>
+    <td width="12" height="114"><p><img src="img/Barra_Parrafo1.png" alt="" width="12" height="75" /></p>
+    <p>&nbsp;</p></td>
+    <td width="527"><p class="gris_texto">Aunque no podemos asegurarle con 100% de seguridad que su servicio podrá llevarse a buen  término, pues pueden presentarse situaciones extraordinarias que lo impidan. Podemos  asegurarle que, en caso de que esto pase, nosotros asumimos los riesgos y por lo tanto, el  cliente recibirá reembolso total de su dinero si Suyo no puede lograr la entrega del producto  final del servicio dentro del plazo de tiempo establecido en el contrato.  Esta política aplica sólo si decide adquirir los servicios bajo la modalidad 2 de servicio  completo (asesoría, análisis, acompañamiento, trámites) (Iva incluido).  Esta política no aplica cuando: </p></td>
+  </tr>
+</table>
+<table width="549" border="0" class="gris_texto">
+  <tr>
+    <td width="12" height="66"><p>&nbsp;</p>
+      <p>&nbsp;</p></td>
+    <td width="527">El tiempo del servicio se extiende por a) casos fortuitos o de fuerza mayor, como paros  judiciales, cierres de las entidades y/o b) el cliente no responde a solicitudes de firmas y  documentación adicional dentro del plazo máximo establecido en tales casos (aplica al  cliente y las otras personas involucradas en el caso).</p></td>
+  </tr>
+  <tr>
+    <td height="18">&nbsp;</td>
+    <td>El cliente entrega documentación o información falsa a Suyo.</td>
+  </tr>
+  <tr>
+    <td height="18">&nbsp;</td>
+    <td>El cliente registra testigos que desconozcan el caso o testigos falsos. </td>
+  </tr>
+  <tr>
+    <td height="34">&nbsp;</td>
+    <td>El cliente aporta información incompleta diferente o nueva información a la aportada  para la elaboración de este diagnóstico. </td>
+  </tr>
+  <tr>
+    <td height="34">&nbsp;</td>
+    <td>El cliente omite información pertinente a Suyo, al juez o a entidades gubernamentales  relacionadas con el proceso.</td>
+  </tr>
+  <tr>
+    <td height="18">&nbsp;</td>
+    <td>El cliente no respeta los plazos de pago establecidos por Suyo al vender el servicio. </td>
+  </tr>
+</table>
+<table width="280" border="0" cellpadding="0" cellspacing="0">
+  <tr>sasa
+    <td width="35" rowspan="3"><img src="img/Barra_Subtitulo.png" alt="" width="23" height="111" /></td>
+    <td width="245"><p class="azul" style="font-size:40px">05<br />
+    </p></td>
+  </tr>
+  <tr>
+    <td><p class="azul" style="font-size:24px; font-weight: bold">TU ALIADO</p></td>
+  </tr>
+  <tr>
+    <td height="23"><p class="gris_texto"><span class="azul" style="font-size:17px; bold">EQUIPO DE TRABAJO</span></p></td>
+  </tr>
+</table>
+<table width="364" border="0" class="azul">
+  <tr>
+    <td style="font-weight:bold">ÉSTE DIAGNÓSTICO SE REALIZÓ CON BASE</td>
+  </tr>
+  <tr>
+    <td>A LA SIGUIENTE INFORMACIÓN</td>
+  </tr>
+</table>
+<p><img src="img/Documento-Diagnostico_03.png" width="358" height="218" /></p>
+<table width="364" border="0" class="azul">
+  <tr>
+    <td style="font-weight:bold">EN ESTE DIAGNÓSTICO</td>
+  </tr>
+  <tr>
+    <td>PARTICIPARON</td>
+  </tr>
+</table>
+<table width="200" border="0">
+  <tr class="azul" style="font-weight:bold">
+    <td colspan="2">ELABORÓ</td>
+    <td colspan="2">APROBÓ</td>
+  </tr>
+  <tr>
+    <td colspan="4"><img src="img/Linea_Tabla.png" width="477" height="4" /></td>
+  </tr>
+  <tr>
+    <td width="18">&nbsp;</td>
+    <td width="217">&nbsp;</td>
+    <td width="13">&nbsp;</td>
+    <td width="217">&nbsp;</td>
+  </tr>
+</table>
+<table width="280" border="0" cellpadding="0" cellspacing="0">
+  <tr>sasa
+    <td width="35" rowspan="3"><img src="img/Barra_Subtitulo.png" alt="" width="23" height="111" /></td>
+    <td width="245"><p class="azul" style="font-size:40px">06<br />
+    </p></td>
+  </tr>
+  <tr>
+    <td><p class="azul" style="font-size:24px; font-weight: bold">ANEXOS</p></td>
+  </tr>
+  <tr>
+    <td height="23"><p class="gris_texto"><span class="azul" style="font-size:17px; bold">DEL DIAGNÓSTICO</span></p></td>
+  </tr>
+</table>
+<table width="364" border="0" class="azul">
+  <tr>
+    <td style="font-weight:bold">COTIZACIÓN</td>
+  </tr>
+  <tr>
+    <td>DETALLADA</td>
+  </tr>
+</table>
+<table width="543" border="0">
+  <tr class="azul" style="font-weight:bold">
+    <td width="461">PERTENENCIA CGP</td>
+    <td width="72">VALOR</td>
+  </tr>
+  <tr class="turquesa" style="font-weight:bold">
+    <td colspan="2">Opción 1:</td>
+  </tr>
+  <tr style="font-size:11px">
+    <td>Asesoría y acompañamiento legal y técnica, incluyendo honorarios  de abogados, ingenieros y/o arquitectos</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr style="background-image:url(img/Pie_Pagina.png); background-repeat:no-repeat; background-position:right; background-size:cover">
+    <td colspan="2">&nbsp;</td>
+  </tr>
+  <tr>
+    <td>Subtotal</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>Retención en la fuente</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>IVA</td>
+    <td>&nbsp;</td>
+  </tr>
+  <tr>
+    <td>PRECIO TOTAL</td>
     <td>&nbsp;</td>
   </tr>
 </table>
 <p>&nbsp;</p>
+<table width="200" border="0">
+  <tr>
+    <td>A
+      <table width="735" height="96" border="0">
+        <tr>
+          <td height="92" class="gris_texto"><p class="azul" style="font-weight:bold">&nbsp;</p></td>
+          <td class="gris_texto"><p class="azul" style="font-weight:bold">Notas Aclaratorias: </p>
+            La presente oferta tiene una validez de 30 días.  El trabajo iniciará a partir de la aprobación de la  cotización y su pago correspondiente.
+            </p></td>
+          <td  class="gris_texto">La presente oferta tiene una validez de 30 días.  El trabajo iniciará a partir de la aprobación de la  cotización y su pago correspondiente.  El trabajo iniciará a partir de la aprobación de la  cotización y su pago correspondiente.</td>
+        </tr>
+    </table></td>
+  </tr>
+</table>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
 <p>&nbsp;</p>
